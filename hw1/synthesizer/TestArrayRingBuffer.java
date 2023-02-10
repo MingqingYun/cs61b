2dc79e59804ce792e6f5e9c24b9987baef5f626e
Mingqing <mingq.yun@gmail.com>
2023-02-09 22:42:52 -0800

    hw1

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,15 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        arb.enqueue(14);
+        arb.enqueue(93);
+        assertEquals(arb.peek(), 14);
+        arb.enqueue(982);
+        assertEquals(arb.dequeue(), 14);
+        assertEquals(arb.dequeue(), 93);
+        assertEquals(arb.isFull(), false);
+        assertEquals(arb.fillCount(), 1);
     }
 
     /** Calls tests for ArrayRingBuffer. */
